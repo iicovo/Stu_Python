3a253f154cac29467c43d5a1a6f4a6be7e2754d5
iicovo <iicovo@163.com>
2021-01-15 09:24:34 +0800

    默认更改列表

--- src/项目/小程序/网易云/downloads.py
+++ src/项目/小程序/网易云/downloads.py
@@ -5,6 +5,11 @@ import re
 import requests
 import time
 
+""" 批量下载歌单歌曲 """
+
+user = ''  # ##这里添加网易账号
+pwd = ''  # ##这里添加网易密码
+
 
 def gain_list(gain_url):
     driver = webdriver.Chrome()
@@ -18,18 +23,20 @@ def gain_list(gain_url):
 
         driver.find_element_by_id('j-official-terms').click()  # 点击同意
 
-        driver.find_element_by_css_selector(
-            '.u-mlg2.u-mlg2-wy').click()  # 网易账户登录
+        driver.find_element_by_css_selector('.u-mlg2.u-mlg2-wy').click()  # 网易账户登录
         time.sleep(1)
+
         username = driver.find_element_by_id('e')
-        username.send_keys('') ### 这里添加网易账号
+        username.send_keys(user)
         time.sleep(1)
+
         kwd = driver.find_element_by_id('epw')
-        kwd.send_keys('') ### 这里添加网易密码
+        kwd.send_keys(pwd)
         time.sleep(1)
-        driver.find_element_by_css_selector(
-            '.js-primary.u-btn2.u-btn2-2').click()
+
+        driver.find_element_by_css_selector('.js-primary.u-btn2.u-btn2-2').click()
         time.sleep(5)
+
     except Exception:
         print('登录失败!')
         pass
@@ -56,18 +63,19 @@ def gain_list(gain_url):
 
 
 def download(count, name):
-    """下载歌曲并保存"""
+    """ 下载歌曲并保存 """
 
     url_download = "http://music.163.com/song/media/outer/url?id=" + str(count)
     headers = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36",
     }
     res = requests.get(url_download, headers=headers)
-    filename = name + '.mp3'
+    filename = name + '.mp3'  # 保存文件名为歌曲名
     try:
         with open(filename, "wb") as f:
             f.write(res.content)
     except Exception:
+        """ 文件名不支持格式异常处理 """
         filename = "未命名歌曲" + count + '.mp3'
         print('*' * 10 + '歌曲名异常' + '*' * 10)
         with open(filename, "wb") as f:
@@ -77,7 +85,7 @@ def download(count, name):
 
 if __name__ == '__main__':
     while True:
-        url = input("请输入要下载的歌单id地址:")
+        url = input("请输入要下载的歌单id地址:")  # 登录网页版查看网页链接后的id数字
         gain_list(url)
         print('下载完成!')
         c = input("是否退出 (y退出):")
