d0e68dd87aeb46803ad625e7fde8da1b8b4d061d
iicovo <iicovo@163.com>
2021-01-15 08:54:52 +0800

    默认更改列表

--- src/项目/小程序/签到/ssr.py
+++ src/项目/小程序/签到/ssr.py
@@ -1,42 +1,59 @@
 # encoding : utf-8
 import time
 from selenium import webdriver
+import requests
 
-# options = webdriver.ChromeOptions()
-# options.add_argument('--headless')
-driver = webdriver.Chrome()
-try:
-    driver.get('https://xxjc.pro')
-    print('获取网页成功!')
-    time.sleep(1)
-except:
-    print('打不开链接,正在尝试更换链接!')
-    driver.get('https://xxjc.vip')
-    print('获取网页成功!')
-    time.sleep(1)
 
-try:
-    # 登录
-    driver.find_element_by_xpath('//*[@id="header"]/nav/ul/li[2]/a').click()
-    inputname = driver.find_element_by_xpath('//*[@id="email"]')
-    inputname.send_keys('599520857@qq.com')
-    inputpwd = driver.find_element_by_xpath('//*[@id="passwd"]')
-    inputpwd.send_keys('kkk00000')
-    driver.find_element_by_xpath('/html/body/div[1]/div/form/div/div[6]/div/div/label/span[3]').click()
-    driver.find_element_by_xpath('//*[@id="login"]').click()
-    time.sleep(2)
-    # 签到
-    try:
-        driver.find_element_by_xpath('//*[@id="checkin"]').click()
-        print('签到成功!')
-    except:
-        print('今天已签到!')
-except:
-    driver.find_element_by_xpath('//*[@id="header"]/nav/ul/li[2]/a').click()
-    time.sleep(2)
-    try:
-        driver.find_element_by_xpath('//*[@id="checkin"]').click()
-        print('签到成功!')
-    except:
-        print('今天已签到!')
-driver.quit()
+class run_program:
+
+    def __init__(self):
+        self.user = "599520857@qq.com"
+        self.pwd = "kkk00000"
+
+    def get_url(self):
+        urls = ["https://xxjc.pro", "https://xxjc.vip"]
+        for i in range(len(urls)):
+            req = requests.get(urls[i])
+            if req.status_code == 200:
+                run_program.Qian_dao(self, urls[i])
+
+    def Qian_dao(self, url):
+        # options = webdriver.ChromeOptions()
+        # options.add_argument('--headless')
+        driver = webdriver.Chrome()
+        driver.get(url)
+        try:
+            # 登录
+            driver.find_element_by_xpath('//*[@id="header"]/nav/ul/li[2]/a').click()
+            inputname = driver.find_element_by_xpath('//*[@id="email"]')
+            inputname.send_keys(self.user)
+            inputpwd = driver.find_element_by_xpath('//*[@id="passwd"]')
+            inputpwd.send_keys(self.pwd)
+            driver.find_element_by_xpath('/html/body/div[1]/div/form/div/div[6]/div/div/label/span[3]').click()
+            driver.find_element_by_xpath('//*[@id="login"]').click()
+            time.sleep(2)
+
+            # 签到
+            try:
+                driver.find_element_by_xpath('//*[@id="checkin"]').click()
+                print('签到成功!')
+            except:
+                pass
+        except:
+            driver.find_element_by_xpath('//*[@id="header"]/nav/ul/li[2]/a').click()
+            time.sleep(2)
+            try:
+                driver.find_element_by_xpath('//*[@id="checkin"]').click()
+                print('签到成功!')
+            except:
+                print('今天已签到!')
+
+        driver.quit()
+
+    def main(self):
+        run_program.get_url(self)
+
+
+if __name__ == '__main__':
+    run = run_program()
+    run.main()
