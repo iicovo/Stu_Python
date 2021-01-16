2b7d26f1996963143ad361e4aa81f65fa99a1725
iicovo <iicovo@163.com>
2021-01-16 09:32:21 +0800

    默认更改列表

--- src/Study/内置函数/t1_sort.py
+++ src/Study/内置函数/t1_sort.py
@@ -1,1 +1,15 @@
 # encoding:utf-8
+
+""" sort """
+# - 参数 (cmp, key, reverse)
+# - key:
+# - reverse: 排序规则，默认FALSE(升序),TRUE为降序
+# - cmp:
+# - 无返回值
+
+ls = ['1', '2', '3', '4', '5']
+
+ls.sort(reverse=True)
+print(ls)
+
+""" lambda """
