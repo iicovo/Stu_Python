39fd297f45fd0093ba971cfb2dc84b417e808063
iicovo <iicovo@163.com>
2021-01-18 10:19:14 +0800

    默认更改列表

--- src/计算机等级考试/stu_包/t6_Pyinstaller.py
+++ src/计算机等级考试/stu_包/t6_Pyinstaller.py
@@ -1,1 +1,15 @@
 # encoding:utf-8
+
+""" 使用 """
+#  Pyinstaller 是命令行执行程序
+# - 1. 在程序文件目录下用 键盘：shift键 + 鼠标：右键 -》 选择 powershell
+# - 2. 在窗口输入命令
+
+
+""" 指令 """
+# - 1. pyinstaller -F <filename.py>   生成文件在dist目录下
+# - 2. -h 查看帮助
+# - 3. --clean 清理打包过程中产生的临时文件
+# - 4. -D/--onedir  默认。生产dist文件夹
+# - 5. -F/--onefile 在dist文件夹中只生成独立的可执行文件
+# - 6. -i <path/iconfilname.ico> 指定打包程序使用的图标文件
